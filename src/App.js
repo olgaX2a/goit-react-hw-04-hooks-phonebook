b7cc3b761c3f4825a22c19c9465d12aa1752991f
olgaX2a <sekretaryova.olga@gmail.com>
2021-08-20 19:45:25 +0300

    HW #4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+import Filter from "./components/Filter";
+import filterContacts from "./helpers/filterContacts";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
+  const [contacts, setContacts] = useLocalStorage("contacts", []);
+  const [filter, setFilter] = useState("");
+
+  const handleAddContact = (contact) => {
+    if (contacts.some((el) => el.name === contact.name)) {
+      alert(contact.name + " is already in contacts");
+      return;
+    }
+    setContacts((prevState) => [...prevState, contact]);
+  };
+
+  const handleDeleteContact = (event) => {
+    setContacts(contacts.filter((contact) => contact.id !== event.target.id));
+  };
+
+  const handleFilterContact = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredContacts = filterContacts(contacts, filter);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter filterValue={filter} onFilter={handleFilterContact} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </>
   );
 }
 
